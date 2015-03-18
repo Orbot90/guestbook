efad91734c2da8e53fb1f18f298ce334fc9dc8b3
Orbot <orbot@localhost.localdomain>
2015-03-18 15:33:39 +0300

    update

--- src/message/Message.java
+++ src/message/Message.java
@@ -29,35 +29,5 @@ public class Message {
         return name;
     }
 
-    public static boolean isReplaceable(String text, String tag) {
-
-        int i = 0;
-        boolean norm = true;
-        int countOpens = 0;
-        int countCloses = 0;
-        if((text.indexOf("[" + tag + "]") > text.indexOf("[/" + tag + "]")) || (text.lastIndexOf("[" + tag + "]") > text.lastIndexOf("[/" + tag + "]"))) {
-            return false;
-        }
-
-        while(i < text.length()) {
-            i = text.substring(i).indexOf("[" + tag + "]");
-            if (i!=-1) {
-                countOpens++;
-            }
-        }
-
-        i = 0;
-
-        while(i < text.length()) {
-            i = text.substring(i).indexOf("[/" + tag + "]");
-            if(i!=-1) {
-                countCloses++;
-            }
-        }
-
-        if(countCloses != countOpens)
-            norm = false;
-
-        return norm;
-    }
+   
 }
